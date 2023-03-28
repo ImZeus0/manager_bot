692bfdfaec3b759957b859fa05af8f62320b2ab6
zeus <zeus1zeus101@gmail.com>
2023-03-28 11:59:12 +0300

    fix test

--- handlers/admin.py
+++ handlers/admin.py
@@ -12,40 +12,17 @@ from aiogram.types.callback_query import CallbackQuery
 from repositories.users import UserRepository
 
 
-@dp.message_handler(commands=['start'], state='*')
-async def mainstart(m: Message, state: FSMContext,users = UserRepository(database)):
-    await state.finish()
-    id = m.chat.id
-    user = await users.get_by_id(id)
-    await m.delete()
-    if user is None:
-        await m.answer('Введите свое ФИО')
-        await Reg.nickname.set()
-    else:
-        await m.answer(f'Привет {user.nickname}',reply_markup=main_keyboard(user.role))
-
-
-@dp.message_handler(state=Reg.nickname)
-async def set_name(m:Message,state:FSMContext,users = UserRepository(database),):
-    await bot.delete_message(m.chat.id, m.message_id-1)
-    await bot.delete_message(m.chat.id, m.message_id)
-    await users.create(m.chat.id,m.text)
-    user = await users.get_by_id(m.chat.id)
-    await m.answer(f'Привет {m.text}', reply_markup=main_keyboard(user.role))
-    await state.finish()
-    create_list(m.text)
-
 
 @dp.callback_query_handler(update_table.filter())
-async def enter_menu(call:CallbackQuery,callback_data:dict,expenses=ExpensesRepository(database)):
-    nickname = callback_data.get('name')
+async def enter_menu(call:CallbackQuery,callback_data:dict,users=UserRepository(database),expenses=ExpensesRepository(database)):
     id_user = int(callback_data.get('id'))
+    user = await users.get_by_id(id_user)
     result = await expenses.get_by_id_user(id_user)
     rows = []
     rows.append(['ID','STATUS','TYPE','SERVICE','AMOUNT','CURRENCY','DATE_ACCEPT'])
     for r in result:
         rows.append([r.id,r.status.value,r.type_operation.value,r.service,r.amount,r.currency,str(r.updated_at)])
-    write_row_spend(nickname,rows)
+    write_row_spend(user.nickname,rows)
     await call.answer('Updated')
 
 
--- keyboards/admin_keyboards.py
+++ keyboards/admin_keyboards.py
@@ -4,6 +4,6 @@ from callback import *
 def users_keyboard(users):
     k = InlineKeyboardMarkup()
     for user in users:
-        k.add(InlineKeyboardButton(user.nickname, callback_data=update_table.new(user.id_user,user.nickname)))
+        k.add(InlineKeyboardButton(user.nickname, callback_data=update_table.new(user.id_user)))
     k.add(InlineKeyboardButton('Назад', callback_data=choose_main_menu.new('back_mainmenu')))
     return k
